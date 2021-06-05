e96d78e00dc23ac25dafc51f423cf9d7069b2fb3
syahmina1819254 <55779851+syahmina1819254@users.noreply.github.com>
2021-06-05 23:10:30 +0800

    add route for payment page

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Router, Scene } from 'react-native-router-flux';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import Dashboard from './src/screens/Dashboard';
+import payment from './src/screens/payment';
 
 export default class App extends Component {
   render() {
@@ -12,6 +13,7 @@ export default class App extends Component {
           <Scene key="LoginScreen" component={LoginScreen} left={()=>null} title="i-Donate" initial={true} />
           <Scene key="SignupScreen" component={SignupScreen} left={()=>null} title="i-Donate" />
           <Scene key="Dashboard" component={Dashboard} left={()=>null} title="i-Donate" />
+          <Scene key="payment" component={payment} left={()=>null} title="i-Donate" />
         </Scene>
       </Router>
     )
