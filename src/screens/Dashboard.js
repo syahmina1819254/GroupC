15764f95794a51591d422ee8cdd4c3b49d217f84
a_sweetscent <athirah.borhan2980@gmail.com>
2021-06-06 00:45:12 +0800

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,67 +1,52 @@
 import React, {Component} from 'react';
 import {View, Button, Text, StyleSheet} from 'react-native';
-<<<<<<< HEAD
 import Slider, {images} from '../screens/Slider';
-=======
-import Slider, {images} from './components/Slider';
->>>>>>> b3be21d5cf6be83230be6df0f00ec912482bba77
+//import Slider, {images} from './components/Slider';
+import { Actions } from 'react-native-router-flux';
 
 const Separator = () => (
   <View style={style.separator} />
 );
 
-<<<<<<< HEAD
-export default class Dashboard extends Component{
-=======
+
+
+
 
 export default class App extends Component{
->>>>>>> b3be21d5cf6be83230be6df0f00ec912482bba77
-
   render(){
     return(
     <View style={{flex: 1, backgroundColor: '#CDEBC8'}}>    
     <View style={style.container}> 
     <Slider images={images}/>
-
    
     </View>
     <View style={style.button}>
     <Button color='#518947' title="Donate Now"
-        onPress={() => Alert.alert('Simple Button pressed')}></Button>
-
+        onPress={()=> Actions.DonateList()}></Button>
 <Separator/>
-
   <Button color='#518947' title="Donation History"
-        onPress={() => Alert.alert('Simple Button pressed')}></Button>
+        onPress={Actions.DonateList()}></Button> {/*Put Donation History Scene Key later*/}
   </View>
   </View>
     )
   }
 }
-
 const style = StyleSheet.create({
   container: {
      marginTop:50, 
   },
-
  button: {
   position: 'absolute',
   alignSelf: 'center',
   width: 272,
-  height: 240,
+  height: 200,
   paddingTop: 20,
   bottom:0,
-
   },
-
    separator: {
     marginVertical: 20,
     borderBottomColor: '#737373',
   },
 
-<<<<<<< HEAD
-})
-=======
 })
 
->>>>>>> b3be21d5cf6be83230be6df0f00ec912482bba77
