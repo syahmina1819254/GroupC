0209c4215e323746de55e349a66f05008b316446
a_sweetscent <athirah.borhan2980@gmail.com>
2021-06-06 00:46:42 +0800

    Add files via upload

---
+++ src/screens/DonateList.js
@@ -0,0 +1,12 @@
+import { Text } from 'native-base';
+import React, { Component } from 'react';
+import { Router, Scene } from 'react-native-router-flux';
+
+
+export default class App extends Component {
+  render() {
+    return (
+     <Text>Hello</Text>
+    )
+  }
+}
