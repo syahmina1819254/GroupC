4c1d024f7cf8c96da52fb722b875f3c0307b924c
aimienadiaa <74192884+aimienadiaa@users.noreply.github.com>
2021-06-07 15:47:58 +0800

    Update DetailsSRA.js

--- src/screens/DetailsSRA.js
+++ src/screens/DetailsSRA.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-
+import { Actions } from 'react-native-router-flux';
 import sra from './components/sra.jpg';
 
 const Separator = () =>(
