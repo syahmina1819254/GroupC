8f4e9db4b45e56d1033d4acf5915bcf8a9db3cb9
aimienadiaa <nadiayusrin3108@gmail.com>
2021-06-07 16:13:15 +0800

    update

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ import SignupScreen from './src/screens/SignupScreen';
 import Dashboard from './src/screens/Dashboard';
 import payment from './src/screens/payment';
 import DonateList from './src/screens/DonateList';
+import DetailsNur from './src/screens/DetailsNur';
+import DetailsNurulIman from './src/screens/DetailsNurulIman';
+import DetailsRTK from './src/screens/DetailsRTK';
+import DetailsRumahEhsan from './src/screens/DetailsRumahEhsan';
+import DetailsSRA from './src/screens/DetailsSRA';
 
 export default class App extends Component {
   render() {
@@ -15,7 +20,12 @@ export default class App extends Component {
           <Scene key="SignupScreen" component={SignupScreen} left={()=>null} title="i-Donate" />
           <Scene key="Dashboard" component={Dashboard} left={()=>null} title="i-Donate" />
           <Scene key="payment" component={payment} left={()=>null} title="i-Donate" />
-           <Scene key="DonateList" component={DonateList} left={()=>null} title="i-Donate"  />
+          <Scene key="DonateList" component={DonateList} left={()=>null} title="i-Donate"  />
+          <Scene key="DetailsNur" component={DetailsNur} left={()=>null} title="i-Donate"  />
+          <Scene key="DetailsNurulIman" component={DetailsNurulIman} left={()=>null} title="i-Donate"  />
+          <Scene key="DetailsRTK" component={DetailsRTK} left={()=>null} title="i-Donate"  />
+          <Scene key="DetailsRumahEhsan" component={DetailsRumahEhsan} left={()=>null} title="i-Donate"  />
+          <Scene key="DetailsSRA" component={DetailsSRA} left={()=>null} title="i-Donate"  />
         </Scene>
       </Router>
     )
