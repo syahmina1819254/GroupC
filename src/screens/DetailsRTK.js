816224ae0132fb911f540ab40fcb92634c86ee8f
aimienadiaa <74192884+aimienadiaa@users.noreply.github.com>
2021-06-07 15:47:15 +0800

    Update DetailsRTK.js

--- src/screens/DetailsRTK.js
+++ src/screens/DetailsRTK.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-
+import { Actions } from 'react-native-router-flux';
 import RTK from './components/RTK.jpg';
 
 const Separator = () =>(
