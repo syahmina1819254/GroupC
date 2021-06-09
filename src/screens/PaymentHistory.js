6d908093291580ca3c0d9199236e6c0474f70b77
Balqis Saharudin <55779824+balqis97@users.noreply.github.com>
2021-06-09 22:09:37 +0800

    Add files via upload

---
+++ src/screens/PaymentHistory.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { removeDonation } from '../services/DataServices';
+import { Alert,View,StyleSheet} from 'react-native';
+import { Content, Text, List, Container } from 'native-base';
+import { db } from '../config/db';
+import PaymentList from '../component/PaymentList';
+
+import * as firebase from 'firebase';
+
+
+export default class PaymentHistyory extends Component {
+  constructor(){
+    super();
+    this.state = {
+    Donation:[],
+  }
+  }
+
+
+  componentDidMount() {
+    let user=firebase.auth().currentUser;
+    var userid=user.uid;
+    let DonationRef = db.ref('/Donation'+userid)
+
+     DonationRef.on('value', (snapshot) => {
+        let data = snapshot.val();
+          if(data){
+            let firebaseData = Object.values(data);
+            this.setState({Donation: firebaseData});
+            console.log(this.state.Donation);
+          }
+     });
+
+  }
+
+  deleteConfirmation = (DonationID) => {
+    Alert.alert(
+      'Status', 
+      'Are you sure you want to delete this Donation?',
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => removeDonation(DonationID)}
+      ],
+      { cancelable: false }
+      );
+  }
+
+
+  render() {
+    const state = this.state;
+    return (
+      <View style={styles.container}>
+
+        <Content padder>
+        <Text style={styles.Header}>DONATION HISTORY</Text>
+        <Container>
+        <List vertical={true}>
+        <PaymentList Donation={this.state.Donation} onLongPress={(DonationID) => {this.deleteConfirmation(DonationID)}} />
+        </List>
+        <Text>{this.props.DonationID}</Text>
+        </Container>
+        </Content>
+
+      </View>
+    );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ecfccb",
+    justifyContent: "center",
+    padding: 10,
+    
+  },
+  Header:{
+    textAlign: "center", 
+    fontSize:25,
+    height: 40, 
+    fontWeight: "bold", 
+    marginTop: 20,
+    marginBottom: 20,
+    color:"#365314"
+  },
+});
