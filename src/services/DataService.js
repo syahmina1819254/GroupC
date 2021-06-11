c966a6597e40f3c2d5f722f340eafced080375d0
Ash <55779756+ashmida@users.noreply.github.com>
2021-06-11 19:10:21 +0800

    Update DataService.js

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -2,18 +2,6 @@ import { db } from '../config/db';
 import { Actions } from 'react-native-router-flux';
 import * as firebase from 'firebase';
 
-export const addUser =  (name,gender,icno,dob,address,email,password) => {
-    db.ref('/users').child(icno).set({
-        name: name,
-        gender: gender,
-        icno: icno,
-        dob: dob,
-        address: address,
-        email: email,
-        password: password
-    }, () => Actions.LoginScreen());
-}
-
 export const addDonation =  (amount, Organisation, PaymentMethod,Date,DonationID) => {
     let user=firebase.auth().currentUser;
     var userid=user.uid;
@@ -24,15 +12,6 @@ export const addDonation =  (amount, Organisation, PaymentMethod,Date,DonationID
         Date: Date,
     }, () => Actions.PaymentHistory());
 }
-// export const updateStudent =  (name, matricno, major, year, status) => {
-//     db.ref('/students').child(matricno).update({
-//         name: name,
-//         matricno: matricno,
-//         major: major,
-//         year: year,
-//         status: status
-//     }, () => Actions.ListScreen());
-// }
 
 export const removeDonation =  (DonationID) => {
     let user=firebase.auth().currentUser;
